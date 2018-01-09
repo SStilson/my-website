7136f6b47e0d7def0c3b27676eb9664717dabe52
SStilson <samantha.stilson@gmail.com>
2018-01-09 14:14:13 -0800

    Fixes resume viewing

---
+++ src/ResumePDF.js
@@ -0,0 +1,25 @@
+var React = require('react');
+var ReactPDF = require('react-pdf/build/entry.webpack');
+var resume = require('./img/resume.pdf');
+var Document = ReactPDF.Document;
+var Page = ReactPDF.Page;
+
+class ResumePDF extends React.Component {
+  state = {
+    pageNumber: 1,
+    scale: 1.2,
+  }
+
+  render() {
+    const { pageNumber, scale } = this.state;
+    return (
+      <div className='resume-pdf'>
+        <Document
+          file={resume}
+        ><Page pageNumber={pageNumber} scale={scale}/>
+        </Document>
+      </div>
+    );
+  }
+}
+module.exports = ResumePDF;
